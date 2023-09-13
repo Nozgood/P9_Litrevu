a63127d09d27521eb504ab7669947fb5f369ce52
nowfeel <nowfeel@bliink.io>
2023-09-13 08:04:51 +0200

    feat: started to create models, need to manage error on image field and User class definition

--- litrevu/settings.py
+++ litrevu/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'litrevu_management',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'litrevu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
+from litrevu import settings
 
-# Create your models here.
+class User(DjangoUser):
+    """Model used to manage User system, herited from django standard User model"""
+    pass
+
+class UserFollows(models.Model):
+    """Model used to manage following system"""
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,  # which model is related to
+        on_delete=models.CASCADE,  # behavior to adopt when the parent model is deleted
+        related_name="following",  # name to use for reverse relation with the target model
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by"
+    )
+
+    class Meta:
+        unique_together = ('user', 'followed_user', )
