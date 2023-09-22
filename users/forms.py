7a8d5152905eb6649af90b66d10c7285f13a31c6
nowfeel <nowfeel@bliink.io>
2023-09-22 14:19:36 +0200

    feat: front for login page, very pas beau

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from users.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
@@ -10,5 +11,12 @@ class SignupForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=64, label="Username")
-    password = forms.CharField(max_length=64, label="Password", widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=64,
+        label="Username",
+    )
+    password = forms.CharField(
+        max_length=64,
+        label="Password",
+        widget=forms.PasswordInput
+    )
