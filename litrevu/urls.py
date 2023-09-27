8f1b4e59779d0ce959e2fba46d6fa6367ea1c36d
nowfeel <nowfeel@bliink.io>
2023-09-27 07:12:08 +0200

    feat: personalised login view

--- litrevu/urls.py
+++ litrevu/urls.py
@@ -22,13 +22,7 @@ import litrevu_management.views
 from users.forms import LoginForm
 
 urlpatterns = [
-    path('login/',
-         LoginView.as_view(
-             template_name="login.html",
-             redirect_authenticated_user=True,
-             authentication_form=LoginForm
-         ),
-         name='login'),
+    path('login/', users.views.user_login, name='login'),
     path('signup/', users.views.signup, name='signup'),
     path('logout/', users.views.logout_user, name='logout'),
     path('', litrevu_management.views.home, name='home'),
--- users/views.py
+++ users/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 import users.forms
 from litrevu import settings
+
+
+def user_login(request):
+    login_form = users.forms.LoginForm(request.POST if request.method == "POST" else None)
+    login_message = ""
+    if request.method == "POST" and login_form.is_valid():
+        user = authenticate(
+            username=login_form.cleaned_data["username"],
+            password=login_form.cleaned_data["password"]
+        )
+        print(f"user: {user}")
+        if user is not None:
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        else:
+            login_message = "identifiants invalides"
+    return render(
+        request,
+        template_name='login.html',
+        context={
+            "form": login_form,
+            "message": login_message,
+        }
+    )
+
 
 def logout_user(request):
     logout(request)
     return redirect('login')
+
 
 def signup(request):
     user_form = users.forms.SignupForm(request.POST if request.method == "POST" else None)
